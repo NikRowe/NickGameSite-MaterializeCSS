5666aa2b7512fa93973fab476279191acd56a506
Nik Rowe <NikRowe7@Gmail.com>
2020-08-27 03:22:32 -0700

    Conditional Link Rendering for Game Cards

--- src/Components/GameCard.js
+++ src/Components/GameCard.js
@@ -10,67 +10,42 @@ const GameCard = ({ item }) => {
       <div className="card-content">
         <span className="card-title">{item.name}</span>
         <p>You can download here</p>
-        <ul>
+        {/* DETERMINES WHICH LINKS TO DISPLAY ON THE MAPPED GAME CARDS */}
+        {item.apple && item.google && item.amazon
+          ? <ul>
+            <li><Link to="/"><img src="https://www.nick.com/apple-store-badge.png" alt="Apple App Store" height="50px" /></Link></li>
+            <li><Link to="/"><img src="https://www.nick.com/google-store-badge.png" alt="Google Play Store" height="50px" /></Link></li>
+            <li><Link to="/"><img src="https://www.nick.com/amazon-store-badge.png" alt="Amazon App Store" height="50px" /></Link></li>
+          </ul>
+          : item.apple && item.google
+            ? <ul>
+              <li><Link to="/"><img src="https://www.nick.com/apple-store-badge.png" alt="Apple App Store" height="50px" /></Link></li>
+              <li><Link to="/"><img src="https://www.nick.com/google-store-badge.png" alt="Google Play Store" height="50px" /></Link></li>
+            </ul>
+            : item.apple && item.amazon
+              ? <ul>
+                <li><Link to="/"><img src="https://www.nick.com/apple-store-badge.png" alt="Apple App Store" height="50px" /></Link></li>
+                <li><Link to="/"><img src="https://www.nick.com/amazon-store-badge.png" alt="Amazon App Store" height="50px" /></Link></li>
+              </ul>
+              : item.google && item.amazon
+                ? <ul>
+                  <li><Link to="/"><img src="https://www.nick.com/google-store-badge.png" alt="Google Play Store" height="50px" /></Link></li>
+                  <li><Link to="/"><img src="https://www.nick.com/amazon-store-badge.png" alt="Amazon App Store" height="50px" /></Link></li>
+                </ul>
+                : item.apple
+                  ? <ul>
+                    <li><Link to="/"><img src="https://www.nick.com/apple-store-badge.png" alt="Apple App Store" height="50px" /></Link></li>
+                  </ul>
+                  : <div />
+        }
+        {/* <ul>
           <li><Link to="/"><img src="https://www.nick.com/apple-store-badge.png" alt="Apple App Store" height="50px" /></Link></li>
           <li><Link to="/"><img src="https://www.nick.com/google-store-badge.png" alt="Google Play Store" height="50px" /></Link></li>
           <li><Link to="/"><img src="https://www.nick.com/amazon-store-badge.png" alt="Amazon App Store" height="50px" /></Link></li>
-        </ul>
+        </ul> */}
       </div>
       <div className="advertisement-bar">Advertisement</div>
     </div>
-
-
-
-
-    // <Card
-    //   className="card small hoverable"
-    //   actions={[
-    //     <>
-    //       <Button
-    //         node="button"
-    //         style={{
-    //           marginRight: '5px'
-    //         }}
-    //         waves="light"
-    //         className="KaiserBg btnH"
-    //       >
-    //         Add Item
-    //       <Icon left>
-    //           add
-    //       </Icon>
-    //       </Button>
-    //       <Button
-    //         node="button"
-    //         waves="light"
-    //         className="KaiserBg btnH"
-    //       >
-    //         Remove Item
-    //       <Icon left>
-    //           remove
-    //       </Icon>
-    //       </Button>
-    //     </>
-    //   ]}
-    //   header={<CardTitle image={item.image} />}
-    //   title={
-    //     <>
-    //       <span className="inventoryItemText" >{item.name} </span>
-    //       <label for={item.id} className="inventoryItemText KaiserText TextInput">{item.totalAmount}</label>
-    //     </>
-    //   }
-
-    //   horizontal
-    // >
-    //   <Col
-    //     m={3}
-    //     s={12}>
-    //     <TextInput
-    //       id={item.id}
-    //       placeholder="0"
-    //       className="TextInput"
-    //     />
-    //   </Col>
-    // </Card>
   );
 };
 
