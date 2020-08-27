0296bad266a18baa031f03c4c19de4024097cd05
Nik Rowe <NikRowe7@Gmail.com>
2020-08-27 14:28:58 -0700

    Scroll Responsive Nav Bar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -16,13 +16,30 @@ class Navbar extends Component {
     render() {
         return (
             <>
-                <nav id="navScrolled" className="navScrolled hide">
-                    <div className="valign-wrapper">
+                {/* RESPONSIVE SCROLL NAV */}
+                <div className="navbar-fixed hide" id="navScrolled" > 
+                    <nav className="navScrolled">
                         <Link to="/" className="brand-logo">
                             <img src="https://nick.mtvnimages.com/nick-assets/common/nick-logo-200x200.png" height="60rem" alt="Nickalodeon Logo" className="logo-img" />
                         </Link>
-                    </div>
-                </nav>
+                        <a href="!#" data-target="Main-Menu" className="sidenav-trigger black-text show-on-medium-and-up">
+                            <i className="material-icons">menu</i>
+                        </a>
+                        <div className="brand-logo center scrolledNavContent" id="nav-content">
+                            <ul className=" hide-on-med-and-down">
+                                <li><Link to="/" className="black-text nav-item">SHOWS</Link></li>
+                                <li><Link to="/" className="black-text nav-item active">GAMES</Link></li>
+                                <li><Link to="/" className="black-text nav-item">APPS</Link></li>
+                                <li><Link to="/" className="black-text nav-item">TV SCHEDULE</Link></li>
+                                <li><Link to="/" className="black-text nav-item">PRIVACY POLICY</Link></li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+
+
+
+                {/* TOP OF PAGE NAV */}
                 <nav id='navbar'>
                     <div className="nav-wrapper">
                         {/* Logo*/}
@@ -32,13 +49,13 @@ class Navbar extends Component {
                             </Link>
                         </div>
                         {/* Sidenav toggle */}
-                        <a href="!#" data-target="Main-Menu" className="sidenav-trigger black-text show-on-medium-and-up">
+                        <a href="!#" data-target="Main-Menu" className="sidenav-trigger black-text show-on-medium-and-up" id="topNavTrigger">
                             <i className="material-icons">menu</i>
                         </a>
                     </div>
                     <div className="nav-content brand-logo center" id="nav-content">
                         <ul className=" hide-on-med-and-down">
-                            <li><Link to="/" className="black-text nav-item" id="showsLink">SHOWS</Link></li>
+                            <li><Link to="/" className="black-text nav-item">SHOWS</Link></li>
                             <li><Link to="/" className="black-text nav-item active">GAMES</Link></li>
                             <li><Link to="/" className="black-text nav-item">APPS</Link></li>
                             <li><Link to="/" className="black-text nav-item">TV SCHEDULE</Link></li>
